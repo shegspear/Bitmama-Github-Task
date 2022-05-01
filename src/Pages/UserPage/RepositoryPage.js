786c8107573b785699035994693d4bf09c5434ee
shegspear <shegspear@gmail.com>
2022-05-01 22:58:38 +0100

    implemented modal in repository page to guide user

--- src/Pages/UserPage/RepositoryPage.js
+++ src/Pages/UserPage/RepositoryPage.js
@@ -8,9 +8,11 @@ import RepositoryComp from '../../Components/Repository/Index';
 
 //testing db
 import testDB from '../../TestingDB/Index';
+import { Modal } from 'react-bootstrap';
 
 function RepositoryPage() {
   const[repoDB, setRepoDB] = useState([]);
+  const [modalShow, setModalShow] = useState(false);
 
   function filterRepo(value) {
     let repoArr = [];
@@ -26,10 +28,31 @@ function RepositoryPage() {
 
   useEffect(() => {
     setRepoDB(testDB);
+    setModalShow(true); 
   }, []);
 
   return (
     <div className='page'>
+
+      {/* hidden guide modal */}
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          size="md"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton></Modal.Header>
+          <Modal.Body>
+            <h4>Feature Alert</h4>
+            <p>
+              Please use the search bar to filter 
+              through the twenty available repository results. <br />
+              Also access your browsers Devtools to visualize the Redux Toolkit. 
+            </p>
+          </Modal.Body>
+        </Modal>
+       {/* hidden guide modal */}
 
      <div className='desktop-header'>
       <HeaderComp currentPage={'repository'} />
