117eada57a59b49f7bc96536d0e03dba681158b1
shegspear <shegspear@gmail.com>
2022-05-01 14:14:50 +0100

    made adjustments to login function in userActions file

--- src/Actions/useraction.js
+++ src/Actions/useraction.js
@@ -12,9 +12,9 @@ import {
     USER_DATA_REQUEST_FAIL
 } from '../Constants/userConstants';
 
-const clientID = process.env.REACT_APP_CLIENT_ID;
-const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
-const redirectUrl = process.env.REACT_APP_REDIRECT_URL;
+// const clientID = process.env.REACT_APP_CLIENT_ID;
+// const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
+// const redirectUrl = process.env.REACT_APP_REDIRECT_URL;
 
 export const login = (code) => async (dispatch) => {
 
@@ -30,13 +30,7 @@ export const login = (code) => async (dispatch) => {
             }
         };
   
-        const {data} = await axios.post('https://github.com/login/oauth/access_token', 
-            {
-                client_id: clientID,
-                client_secret: clientSecret,
-                code: code,
-                redirect_uri: redirectUrl,
-            },
+        const {data} = await axios.post(`https://github.com/login/oauth/access_token?client_id=f57fc45a6dd209a0b880?client_secret=09d76a78818935dbd508895e7ea05821aae4a8f8?code=${code}?redirect_uri=https://dreamy-lamington-1f6400.netlify.app/repository`, 
             config
         );
   
@@ -70,7 +64,7 @@ export const githubUserRequest = () => async(dispatch) => {
             }
         };
 
-        const {data} = await axios.get(`https://github.com/login/oauth/authorize?client_id=f57fc45a6dd209a0b880`);
+        const {data} = await axios.get(`https://github.com/login/oauth/authorize?client_id=f57fc45a6dd209a0b880`, config);
   
         dispatch({
             type: GITHUB_USER_REQUEST_SUCCESS,
